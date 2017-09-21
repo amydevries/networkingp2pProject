aab1882c0341e7709bd2e8c6e092b7141da449b4
Unknown <coltonmoler@gmail.com>
2017-09-21 12:56:16 -0400

    fixed logger and added tests

--- src/Logger/PeerLogger.java
+++ src/Logger/PeerLogger.java
@@ -1,75 +1,134 @@
 package Logger;
 
 
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class PeerLogger {
 
-    private PrintWriter logger;
+    private FileWriter logger;
 
     public void setup(int peerID){
 
         try{
-            logger = new PrintWriter("log_peer_"+peerID+".log");
+            logger = new FileWriter("log_peer_"+peerID+".log");
 
 
         }catch(Exception e) {}
     }
 
+    public void close(){
+        try{
+            logger.close();
+        }catch (Exception e) {}
+    }
+
     private String getTimeStamp(){
-       return new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+       return new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss").format(Calendar.getInstance().getTime());
     }
 
     public void tcpConnection(int peerGeneratingLog, int peerConnectedTo){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerGeneratingLog + " makes a connection to Peer " + peerConnectedTo);
+
+        try {
+            logger.write(timeStamp+": Peer " + peerGeneratingLog + " makes a connection to Peer " + peerConnectedTo);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void changePreferredNeighbors(int peerID, int[] preferredNeighborList){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerID + " has the preferred neighbors " + preferredNeighborList);
+        try {
+            logger.write(timeStamp+": Peer " + peerID + " has the preferred neighbors " + preferredNeighborList);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void changeOptimisticallyUnchockedNeighbor(int peerID, int optimisticallyUnchokedNeighbor){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerID + " has the optimistically unchoked neighbor " + optimisticallyUnchokedNeighbor);
+        try {
+            logger.write(timeStamp+": Peer " + peerID + " has the optimistically unchoked neighbor " + optimisticallyUnchokedNeighbor);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void unchoking(int peerUnchoked, int peerWhoUnchokes){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerUnchoked + " is unchoked by " + peerWhoUnchokes);
+        try {
+            logger.write(timeStamp+": Peer " + peerUnchoked + " is unchoked by " + peerWhoUnchokes);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void choking(int peerChoked, int peerWhoChokes){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerChoked + " is choked by " + peerWhoChokes);
+        try {
+            logger.write(timeStamp+": Peer " + peerChoked + " is choked by " + peerWhoChokes);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void receivedHaveMessage(int peerReceivedMessage, int peerSentMessage, int pieceIndex){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerReceivedMessage + " received the ‘have’ message from " + peerSentMessage + " for the piece " + pieceIndex);
+        try {
+            logger.write(timeStamp+": Peer " + peerReceivedMessage + " received the ‘have’ message from " + peerSentMessage + " for the piece " + pieceIndex);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void receivedInterestedMessage(int peerReceivedMessage, int peerSentMessage){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerReceivedMessage + " received the ‘interested’ message from " + peerSentMessage);
+        try {
+            logger.write(timeStamp+": Peer " + peerReceivedMessage + " received the ‘interested’ message from " + peerSentMessage);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void receivedNotInterestedMessage(int peerReceivedMessage, int peerSentMessage){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerReceivedMessage + " received the ‘not interested’ message from " + peerSentMessage);
+        try {
+            logger.write(timeStamp+": Peer " + peerReceivedMessage + " received the ‘not interested’ message from " + peerSentMessage);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void downloadingPiece(int peerDownloadedPiece, int peerSentPiece, int pieceIndex, int numberOfPieces){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerDownloadedPiece + " has downloaded the piece " + pieceIndex + " from " +
-                peerSentPiece + "Now the number of pieces it has is " + numberOfPieces);
+        try {
+            logger.write(timeStamp+": Peer " + peerDownloadedPiece + " has downloaded the piece " + pieceIndex + " from " +
+                    peerSentPiece + "Now the number of pieces it has is " + numberOfPieces);
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void completedDownload(int peerID){
         String timeStamp= getTimeStamp();
-        logger.println(timeStamp+": Peer " + peerID + "has downloaded the complete file.");
+        try {
+            logger.write(timeStamp+": Peer " + peerID + "has downloaded the complete file.");
+            logger.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- test/Logger/PeerLoggerTest.java
+++ test/Logger/PeerLoggerTest.java
@@ -1,11 +1,82 @@
 package Logger;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class PeerLoggerTest{
 
-    @Test
-    public void test01(){
+    private final int PEER_ID = -1;
+    private final String fileName = "log_peer_" + PEER_ID + ".log";
+    private PeerLogger logger;
 
+    @BeforeEach
+    public void setUp(){
+        logger = new PeerLogger();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        logger.close();
+        try {
+            Path path = Paths.get(fileName);
+            Files.delete(path);
+        } catch (Exception e) {}
+    }
+
+    @Test
+    public void test01_LoggerSetupCreatesFile(){
+        // Act
+        logger.setup(PEER_ID);
+        File file = new File(fileName);
+
+        // Assert
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void test02_LoggerSetupCreatesFileWithRightName(){
+        // Act
+        logger.setup(PEER_ID);
+        File file = new File("log_peer_" + (PEER_ID+1) + ".log");
+
+        // Assert
+        assertFalse(file.exists());
+    }
+
+    @Test
+    public void test03_tcpConnectionPrintsCorrectly(){
+        // Arrange
+        String line = null;
+        int peerGeneratingLog = 1;
+        int peerConnectedTo = 2;
+
+        // Act
+        logger.setup(PEER_ID);
+        logger.tcpConnection(peerGeneratingLog, peerConnectedTo);
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            line = bufferedReader.readLine();
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(Exception e) { }
+
+        // Assert
+        assertTrue(line.contains(": Peer " + peerGeneratingLog + " makes a connection to Peer " + peerConnectedTo));
     }
 }
