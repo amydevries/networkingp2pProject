564b798e7cf638f8b301d1bef3da2d685e68a795
Unknown <amy.devries@ufl.edu>
2017-10-22 16:19:26 -0400

    Hashtable for PeerConnections
    boom

--- src/DefaultProcesses/startRemotePeers.java
+++ src/DefaultProcesses/startRemotePeers.java
@@ -34,13 +34,13 @@ public class startRemotePeers {
                 System.out.println("Start remote peer " + peerInfoReader.getPeerIDS(i) +  " at " + peerInfoReader.getPeerHostNames(i) );
 
                 //the line below is what is supposed to work to connect to ssh
-                Runtime.getRuntime().exec("ssh " + peerInfoReader.getPeerHostNames(i) + " cd " + path + "; java peerProcess " + peerInfoReader.getPeerIDS(i));
+                //Runtime.getRuntime().exec("ssh " + peerInfoReader.getPeerHostNames(i) + " cd " + path + "; java peerProcess " + peerInfoReader.getPeerIDS(i));
 
                 //the lines below is playing with stuff to see if I can get it to just run on our machines for the time being
                 //Runtime.getRuntime().exec("java peerProcess " + peerInfoReader.getPeerIDS(i));
-                /*ProcessBuilder pb = new ProcessBuilder("cmd.exe", "java peerProcess " + peerInfoReader.getPeerIDS(i));
+                ProcessBuilder pb = new ProcessBuilder("cmd.exe", "java peerProcess " + peerInfoReader.getPeerIDS(i));
                 Process process = pb.start();
-                process.waitFor();*/
+                process.waitFor();
                 //Process myProcess = Runtime.getRuntime().exec("peerProcess.java " + peerInfoReader.getPeerIDS(i));
             }
             System.out.println("All remote peers started." );
