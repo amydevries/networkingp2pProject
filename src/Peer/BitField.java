30a5b462ad39903cafafd053fef614179bd67734
Colton <coltonmoler@gmail.com>
2017-10-22 16:24:12 -0400

    finished bitfield

--- src/Peer/BitField.java
+++ src/Peer/BitField.java
@@ -1,28 +1,64 @@
 package Peer;
-import java.util.BitSet;
+
+import java.util.ArrayList;
 
 public class BitField {
 
-    private BitSet bits;
+    private byte[] bitfield;
+    private int numBits;
 
-
-    public BitField(BitSet bits) {
-        this.bits = bits;
+    public BitField(int numBits){
+        this(numBits, false);
     }
 
-    public BitField(byte bytes[]) {
-        this.bits = bits;
+    public BitField(int numBits, boolean hasFile){
+        createNewBitField(numBits, hasFile);
     }
 
-    // TODO: test if this is working properly
-    public byte[] toByteArray() {
-        byte[] bytes = new byte[(int) Math.ceil(bits.length() / 8.)];
-        for (int i = 0; i < bits.length(); i++) {
-            if (bits.get(i)) {
-                bytes[i/8] |= 1 << (i % 8);
-            }
+    private void createNewBitField(int size, boolean hasFile){
+        bitfield = new byte[size];
+
+        for(int i = 0; i < bitfield.length; ++i){
+            bitfield[i] = (byte)(hasFile ? 1 : 0);
         }
-        return bytes;
     }
 
+    public ArrayList<Integer> compareTo(byte[] bitfieldComparingTo){
+        ArrayList<Integer> interestingPieces = new ArrayList<Integer>();
+
+        for(int i = 0; i < bitfieldComparingTo.length; ++i)
+            if((bitfield[i] == (byte) 0) && (bitfieldComparingTo[i] == (byte)1))
+                interestingPieces.add(i);
+
+        return interestingPieces;
+    }
+
+    public void getPiece(int index){
+        bitfield[index] = (byte)1;
+    }
+
+    public Boolean isFull(){
+
+        for(byte current: bitfield){
+            if(current == (byte) 0) return false;
+        }
+
+        return true;
+    }
+
+    public byte[] getBitfield() {
+        return bitfield;
+    }
+
+    public void setBitfield(byte[] bitfield) {
+        this.bitfield = bitfield;
+    }
+
+    public int getNumBits() {
+        return numBits;
+    }
+
+    public void setNumBits(int numBits) {
+        this.numBits = numBits;
+    }
 }
